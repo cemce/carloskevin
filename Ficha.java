3d2562c88ef02943e5927361f8721a41e04e59d7
cemce <47115581+cemce@users.noreply.github.com>
2020-11-11 20:12:26 +0100

    Update Ficha.java

--- Ficha.java
+++ Ficha.java
@@ -1,84 +1,94 @@
+public class Ficha {
 
-public class Tablero {
-
-	Ficha[][] tablero = new Ficha[8][8];
-
-	Tablero () {
+	String color;
+	String letra;
+	
+	public Ficha (String color, String letra) {
+	 
+		this.color = color;
+		this.letra = letra;
 		
-		for (int filas = 0; filas < tablero.length; filas++) {
-			for(int columnas = 0; columnas < tablero.length; columnas++) {
+	}
+	
+	public boolean posibleMoviment (int antigaX, int antigaY, int x, int y) {
+		
+		//Torre posible movimientos
+		if (this.letra.equalsIgnoreCase("t")) {
+			
+			if (x == antigaX || antigaY == y) {
+				System.out.println("correcte");
+				return true;
+			}
+			//Comprobant de on les ha posat
+			else {
+				System.out.println(antigaX + "" + x);
+			}
+		}
+		
+		//Peones movimientos possibles
+		if (this.letra.equalsIgnoreCase("p")) {
+			
+			if (this.color.equalsIgnoreCase("blancas")) {
 				
-				if (filas == 0) {
-					
-					tablero[0][0] = new Ficha	("negro", "t");
-					tablero[0][1] = new Ficha("negro", "c");
-					tablero[0][2] = new Ficha("negro", "a");
-					tablero[0][3] = new Ficha("negro", "k");
-					tablero[0][4] = new Ficha("negro", "q");
-					tablero[0][5] = new Ficha("negro", "a");
-					tablero[0][6] = new Ficha("negro", "c");
-					tablero[0][7] = new Ficha("negro", "t");
+				if ( (x+1 == antigaX || x+2 == antigaX) && antigaY == y ) {
+					System.out.println("correcte");
+					return true;
 				}
-				else if (filas == 1) {
-
-					for (columnas = 0; columnas < tablero.length; columnas++) {
-						tablero[filas][columnas] = new Ficha("negro", "p");
-					}
-				}
-				else if(filas == 6) {
-					
-					for (columnas = 0; columnas < tablero.length; columnas++) {
-						tablero[filas][columnas] = new Ficha("blancas", "P");
-					}
-				}
-				else if(filas == 7) {
-					
-					tablero[7][0] = new Ficha("blancas", "T");
-					tablero[7][1] = new Ficha("blancas", "C");
-					tablero[7][2] = new Ficha("blancas", "A");
-					tablero[7][3] = new Ficha("blancas", "K");
-					tablero[7][4] = new Ficha("blancas", "Q");
-					tablero[7][5] = new Ficha("blancas", "A");
-					tablero[7][6] = new Ficha("blancas", "C");
-					tablero[7][7] = new Ficha("blancas", "T");
-					}
 				else {
-					
-					tablero[filas][columnas] = new Ficha("empty","*");
+					System.out.println("peon fallo movimiento");
+					return false;
 				}
 			}
-		}
-		showTablero();
-		
-	}
-		
-		
-	
-	
-	public void showTablero () {
-		
-		for (int filas = 0; filas < tablero.length; filas++) {
-			for (int columnas = 0; columnas < tablero.length; columnas++) {
-				System.out.print(tablero[filas][columnas].letra + " ");
+			
+			else {
+			if ( (x-1 == antigaX || x-2 == antigaX) && antigaY == y ) {
+				System.out.println("correcte");
+				return true;
 			}
-			System.out.println("");
+			else {
+				System.out.println("peon fallo movimiento");
+				return false;
+			}
+		}
+	}//Fin peon
+		
+		if (this.letra.equalsIgnoreCase("c")) {
+			
+			if ((x - antigaX) * (x- antigaX) + (y - antigaY) * (y- antigaY) == 5) {
+				System.out.println("correcte");
+				return true;
+			}
+			else {
+				System.out.println("fallo movimiento caballo");
+				return false;
+			}
+			
+		} //Fin del caballo
+		
+		if (this.letra.equalsIgnoreCase("q")) {
+			
+			if ((x == antigaX) || (y == antigaY) || (Math.abs(x - antigaX) == Math.abs(y - antigaY))) {
+				System.out.println("correcte");
+				return true;
+			}
+			else {
+				System.out.println("fallo movimiento reina");
+				return false;
+			}
+		} //Fin de la reina
+		
+		if (this.letra.equalsIgnoreCase("k")) {
+			
+			if ( (Math.abs(x - antigaX )<=1) && (Math.abs(y - antigaY) <=1) ) {
+				System.out.println("correcte");
+				return true;
+			}
+			else {
+				System.out.println("fallo movimiento rei");
+				return false;
+			}
+		}
+		return false;
 		}
 	}
-	
-	public void movimiento (int antigaX, int antigaY, int x, int y) {
-		
-		// if compleix totes les condicions, encara falten per posar
-		if (tablero[antigaX][antigaY].posibleMoviment(antigaX, antigaY, x, y)) {
-		
-		tablero[x][y].color = tablero[antigaX][antigaY].color;
-		tablero[x][y].letra = tablero[antigaX][antigaY].letra;	
-		
-		tablero[antigaX][antigaY].color = "empty";
-		tablero[antigaX][antigaY].letra = "*";
-		
-	
-		}
-		showTablero();
-	}
-	
-}
+
