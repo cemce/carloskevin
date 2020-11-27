b4e88c7b6f1ce5daa1eec0857cda857c770cf40f
cemce <47115581+cemce@users.noreply.github.com>
2020-11-27 13:37:02 +0100

    Update Tablero.java

--- Tablero.java
+++ Tablero.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class Tablero {
 
+	//Indicamos atributos de la clase
+	
+	//El tablero estará compuesto de fichas en cada posición
 	Ficha[][] tablero = new Ficha[8][8];
+	//Tendrá 2 reyes, si uno desaparece, ganará x o y
 	int reyes = 2;
-	String colorRei1 = "";
-	String colorRei2 = "";
+	
+	//Los colores de los reyes de la partida
+	String colorRei1;
+	String colorRei2;
 	
 
 	Tablero () {
 		
+		//Empezamos a rellenar el tablero
 		for (int filas = 0; filas < tablero.length; filas++) {
 			for(int columnas = 0; columnas < tablero.length; columnas++) {
 				
+				//Aquí indicaremos todas las fichas negras que no sean peones
 				if (filas == 0) {
 					
 					tablero[0][0] = new Ficha	("negro", "t");
@@ -24,18 +32,22 @@ public class Tablero {
 					tablero[0][6] = new Ficha("negro", "c");
 					tablero[0][7] = new Ficha("negro", "t");
 				}
+				//Aqui los peones ya que estaran delante de las otras piezas
 				else if (filas == 1) {
 
 					for (columnas = 0; columnas < tablero.length; columnas++) {
 						tablero[filas][columnas] = new Ficha("negro", "p");
 					}
 				}
+				// Aquí indicaremos las fichas blancas, que estarán en lo más alto de la matriz.
+				//Los peones
 				else if(filas == 6) {
 					
 					for (columnas = 0; columnas < tablero.length; columnas++) {
 						tablero[filas][columnas] = new Ficha("blancas", "P");
 					}
 				}
+				//Llenaremos las fichas blancas como antes, pero cambiando el numero de fila y los atributos de las mismas
 				else if(filas == 7) {
 					
 					tablero[7][0] = new Ficha("blancas", "T");
@@ -48,18 +60,19 @@ public class Tablero {
 					tablero[7][7] = new Ficha("blancas", "T");
 					}
 				else {
-					
+					//En todas las otras premisas, este será el objeto que se pondrá en el tablero
 					tablero[filas][columnas] = new Ficha("empty","*");
 				}
 			}
 		}
+		//Mostramos tablero
 		showTablero();
 		
 	}
 		
 		
 	
-	
+	//Mètodo para mostrar tablero, simplemente.
 	public void showTablero () {
 		
 		int contador = 0;
@@ -67,6 +80,7 @@ public class Tablero {
 			for (int columnas = 0; columnas < tablero.length; columnas++) {
 				System.out.print(tablero[filas][columnas].letra + " ");
 				
+				//Aquí dentro también iremos contando los reyes, para saber el ganador
 				if (tablero[filas][columnas].letra.equals("k")) {
 					contador++;
 					this.reyes = contador;
@@ -86,25 +100,25 @@ public class Tablero {
 	
 	public boolean movimiento (int antigaX, int antigaY, int x, int y,int turno) {
 		
-		// if compleix totes les condicions, encara falten per posar
-		
+	
+		//Depende de los turnos, si no se coge la ficha indicada, retornamos falso, para repetir el paso
 		if (turno == 1 && !tablero[antigaX][antigaY].color.equalsIgnoreCase("blancas")) {
 			System.out.println("Malament, has de escollir les teves fitxes");
 			showTablero();
 			return false;
 			
 		}
-		
+		//Depende de los turnos, si no se coge la ficha indicada, retornamos falso, para repetir el paso
 		else if (turno != 1 && !tablero[antigaX][antigaY].color.equalsIgnoreCase("negro")) {
 			System.out.println("Malament, has de escollir les teves fitxes");
 			showTablero();
 			return false;
 		}
 		
-		
+		//Aquí en este metodo veremos si los dos mètodos son correctos, las comprobaciones idoneas
 		else if (tablero[antigaX][antigaY].posibleMoviment(tablero,antigaX, antigaY, x, y) && sePuedeMover(antigaX, antigaY, x, y)) {
 		
-			
+			//Si las fichas indicadas son del mismo color, retornamos falso
 			if (tablero[antigaX][antigaY].color.equalsIgnoreCase(tablero[x][y].color)) {
 				
 				System.out.println("No puedes moverla ahí, el color es el mismo al de la ficha");
@@ -112,16 +126,8 @@ public class Tablero {
 				return false;
 			}
 			
-			//Controlar el peon que no pueda matar delante
-			else if (tablero[antigaX][antigaY].letra.equalsIgnoreCase(tablero[x][y].letra) && 
-					(tablero[antigaX][antigaY].letra.equalsIgnoreCase("p")
-							|| tablero[x][y].letra.equalsIgnoreCase("p"))) {
-				
-				System.out.println("Peon no mata así");
-				showTablero();
-				return false;
-			}
 			
+			//En el caso que no se cumpla el anterior, cambiamos las dos posiciones, intercambiamos todos los atributos
 			else {
 			tablero[x][y].color = tablero[antigaX][antigaY].color;
 			tablero[x][y].letra = tablero[antigaX][antigaY].letra;	
@@ -131,8 +137,9 @@ public class Tablero {
 			}
 	
 		}
+		//Si la comprobacion de los dos pasos ha sido incorrecta, retornamos el debido boolean y enseñamos el tablero
 		else {
-			System.out.println("Algo ha fallat " + "comprobació moviment peça " + tablero[antigaX][antigaY].posibleMoviment(tablero,antigaX, antigaY, x, y) + "comprobació pot passar " + sePuedeMover(antigaX, antigaY, x, y));
+		
 			showTablero();
 			return false;
 		}
@@ -140,7 +147,7 @@ public class Tablero {
 		return true;
 	}
 	
-	//Aqui hay errores 100%
+	
 	public boolean sePuedeMover(int antigaX, int antigaY, int x, int y) {
 	    
 		
@@ -149,7 +156,7 @@ public class Tablero {
 			//Es va restant la i, comenc                             ESQUERRA
 			for (int i = antigaY - 1; i > y; i--) {
 	            if (!tablero[i][antigaX].letra.equalsIgnoreCase("*")) {
-	               System.out.println("esquerra mal");
+	              
 	            	return false;
 	               }
 			}
@@ -161,7 +168,7 @@ public class Tablero {
 			//Se suma les files 
 		        for (int i = antigaX + 1; i < x; i++) {
 		            if (!tablero[i][antigaY].letra.equalsIgnoreCase("*")) {
-		            	System.out.println("abaix mal");
+		            	
 		                return false;
 		            }
 		        }
@@ -172,7 +179,7 @@ public class Tablero {
 			//Sumem a la columna per veure si trobem algo que no toca
 	        for (int i = antigaY + 1; i < y; i++) {
 	            if (!tablero[antigaX][i].letra.equalsIgnoreCase("*")) {
-	            	System.out.println("dreta malament");	            	
+	            	            	
 	            	return false;
 	               
 	            }
@@ -184,59 +191,70 @@ public class Tablero {
 		else if (antigaX > x && antigaY == y) { // horizontal west
 	        for (int i = antigaX - 1; i > x; i--) {
 	            if (!tablero[i][antigaX].letra.equalsIgnoreCase("*")) {
-	            	System.out.println("amunt mal");
+	            	
 	                return false;
 	            }
 	        }
 	    }
 		
-		return true;
-		/*//Posició fila inicial és igual a la fila, sol canvia columnes
+		//Diagonal hacia abajo derecha
+		else if (antigaX < x && antigaY < y) {
+			for (int i = antigaX +1; i < x; i++) {
+				antigaY++;
+				 if (!tablero[i][antigaY].letra.equalsIgnoreCase("*")) {
+					 
+		            	System.out.println("hola");
+		                return false;
+		            }
+			}
+		}
 		
-	    else if (antigaX < x && antigaY > y) { 
-	        
-	        for (int row = antigaX + 1; row < x; row++) {
-	            for (int col = antigaY - 1; col >= y; col--) {
-	                if (!tablero[row][antigaX].letra.equalsIgnoreCase("*")) {
-	                	System.out.println("vertical mal");
-	                    return false;
-	                }
-	            }
-	        }
-	    } else if (antigaX < x && antigaY < y) { 
-	        for (int row = antigaX + 1; row < x; row++) {
-	            for (int col = antigaY + 1; col < y; col++) {
-	                if (!tablero[row][antigaX].letra.equalsIgnoreCase("*")) {
-	                	System.out.println("vertical mal");
-	                    return false;
-	                }
-	            }
-	        }
-	    } else if (antigaX > x && antigaY < y) {
-	        for (int row = antigaX - 1; row >= x; row--) {
-	            for (int col = antigaY + 1; col < y; col++) {
-	                if (!tablero[row][antigaX].letra.equalsIgnoreCase("*")) {
-	                	System.out.println("vertical mal");
-	                    return false;
-	                }
-	            }
-	        }
-	    } else if (antigaX > x && antigaY > y) { 
-	        for (int row = antigaX - 1; row >= x; row--) {
-	            for (int col = antigaY - 1; col >= y; col--) {
-	                if (!tablero[row][antigaX].letra.equalsIgnoreCase("*")) {
-	                	System.out.println("vertical mal");
-	                    return false;
-	                }
-	            }
-	        }
-	    }
-	    return true;*///
+		//Diagonal hacia abajo izquierda
+	
+		else if (antigaX < x && antigaY > y) {
+			for (int i = antigaX +1; i < x; i++) {
+				antigaY--;
+				 if (!tablero[i][antigaY].letra.equalsIgnoreCase("*")) {
+					 
+		            	
+		                return false;
+		          }
+			}
+		}
+		
+		else if (antigaX > x && antigaY < y) {
+			for (int i = antigaX - 1; i > x; i--) {
+				antigaY++;
+				 if (!tablero[i][antigaY].letra.equalsIgnoreCase("*")) {
+					 
+		            	
+		                return false;
+		          }
+			}
+		}
+		
+		else if (antigaX > x && antigaY > y) {
+			for (int i = antigaX - 1; i > x; i--) {
+				antigaY--;
+				 if (!tablero[i][antigaY].letra.equalsIgnoreCase("*")) {
+					 
+		            	
+		                return false;
+		          }
+			}
+		}
+		
+		return true;
+
 	}
+	
+	//Funcion final del juego
 	public boolean end (Tablero tablero, int turno) {
 		
+		//Si no hay 2 reyes entramos al bucle para ver quien gana
 		if (tablero.reyes != 2) {
 			
+			//Si el turno es uno, porque no llega a resetearse indicamos los ganadores cruzados 1.negras 2.blancas
 			if (turno == 1) {
 				System.out.println("Han ganado las negras");
 			}
